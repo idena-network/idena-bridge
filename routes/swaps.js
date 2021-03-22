07d14fb39749f44721272f673b58ccef725575d2
RealMahmoud <realmahmoudm@gmail.com>
2021-03-22 18:55:10 +0200

    update

--- routes/swaps.js
+++ routes/swaps.js
@@ -9,7 +9,9 @@ const {
 const {
     ethers
 } = require('ethers');
-const logger = require('../logger').child({component: "api"})
+const logger = require('../logger').child({
+    component: "api"
+})
 
 router.get('/latest', async function (req, res) {
     try {
@@ -73,23 +75,25 @@ async function info(req, res) {
         });
 }
 
-router.post('/assign', async function (req, res) {
+router.get('/assign', async function (req, res) {
     try {
         await assign(req, res)
     } catch (error) {
-        let requset_uuid = req.body.uuid || req.query.uuid;
-        logger.error(`Failed ${req.path} (uuid=${requset_uuid}): ${error}`)
+        logger.error(`Failed ${req.path} (uuid=${req.query.uuid}): ${error}`)
         res.sendStatus(500)
     }
 });
 
 async function assign(req, res) {
-    let requset_uuid = req.body.uuid || req.query.uuid;
-    const reqInfo = `${req.path} (uuid=${requset_uuid}, tx=${req.body.tx})`
+    const reqInfo = `${req.path} (uuid=${req.query.uuid}, tx=${req.query.tx})`
     logger.debug(`Got ${reqInfo}`)
-    if (!uuid.validate(requset_uuid)) {
+    if (!uuid.validate(req.query.uuid)) {
         logger.debug(`Bad request ${reqInfo}`)
-        res.sendStatus(400);
+        if (req.query.no_redirect) {
+            res.sendStatus(400);
+        } else {
+            res.redirect(`/operation/${req.query.uuid}`);
+        }
         return
     }
 
@@ -102,19 +106,24 @@ async function assign(req, res) {
     let sql = "SELECT `uuid`,`amount`,`address`,`type`,`idena_tx`,`bsc_tx`, `time` FROM `swaps` WHERE `uuid` = ? LIMIT 1;";
     let data
     try {
-        [data] = await conP.execute(sql, [requset_uuid]);
+        [data] = await conP.execute(sql, [req.query.uuid]);
     } catch (err) {
         reject(err)
         return
     }
 
-    if (data[0] && data[0].type === 0 && !(data[0].idena_tx) && ethers.utils.isHexString(req.body.tx) && req.body.tx.length === 66) {
-        if (await idena.isTxExist(req.body.tx)) {
-            if (await idena.isValidSendTx(req.body.tx, data[0].address, data[0].amount, data[0].time) && await idena.isNewTx(req.body.tx)) {
+    if (data[0] && data[0].type === 0 && !(data[0].idena_tx) && ethers.utils.isHexString(req.query.tx) && req.query.tx.length === 66) {
+
+        if (await idena.isTxExist(req.query.tx)) {
+            if (await idena.isValidSendTx(req.query.tx, data[0].address, data[0].amount, data[0].time) && await idena.isNewTx(req.query.tx)) {
                 sql = "UPDATE `swaps` SET `idena_tx` = ? WHERE `uuid` = ? ;";
-                conP.execute(sql, [req.body.tx, requset_uuid]).then(() => {
+                conP.execute(sql, [req.query.tx, req.query.uuid]).then(() => {
                     logger.debug(`Completed ${reqInfo}`)
-                    res.sendStatus(200);
+                    if (req.query.no_redirect) {
+                        res.sendStatus(200);
+                    } else {
+                        res.redirect(`/operation/${req.query.uuid}`);
+                    }
                 }).catch(reject)
                 return
             }
@@ -122,20 +131,29 @@ async function assign(req, res) {
             res.sendStatus(400);
             return
         }
+
         sql = "UPDATE `swaps` SET `idena_tx` = ? WHERE `uuid` = ?;";
-        conP.query(sql, [req.body.tx, requset_uuid]).then(() => {
+        conP.query(sql, [req.query.tx, req.query.uuid]).then(() => {
             logger.debug(`Completed ${reqInfo}`)
-            res.sendStatus(200);
+            if (req.query.no_redirect) {
+                res.sendStatus(200);
+            } else {
+                res.redirect(`/operation/${req.query.uuid}`);
+            }
         }).catch(reject)
         return
     }
-    if (data[0] && data[0].type === 1 && !(data[0].bsc_tx) && ethers.utils.isHexString(req.body.tx) && req.body.tx.length === 66) {
-        if (await bsc.isTxExist(req.body.tx)) {
-            if (await bsc.isValidBurnTx(req.body.tx, data[0].address, data[0].amount, data[0].time) && await bsc.isNewTx(req.body.tx)) {
+    if (data[0] && data[0].type === 1 && !(data[0].bsc_tx) && ethers.utils.isHexString(req.query.tx) && req.query.tx.length === 66) {
+        if (await bsc.isTxExist(req.query.tx)) {
+            if (await bsc.isValidBurnTx(req.query.tx, data[0].address, data[0].amount, data[0].time) && await bsc.isNewTx(req.query.tx)) {
                 sql = "UPDATE `swaps` SET `bsc_tx` = ? WHERE `uuid` = ?;";
-                conP.query(sql, [req.body.tx, requset_uuid]).then(() => {
+                conP.query(sql, [req.query.tx, req.query.uuid]).then(() => {
                     logger.debug(`Completed ${reqInfo}`)
-                    res.sendStatus(200);
+                    if (req.query.no_redirect) {
+                        res.sendStatus(200);
+                    } else {
+                        res.redirect(`/operation/${req.query.uuid}`);
+                    }
                 }).catch(reject)
                 return
             }
@@ -144,9 +162,13 @@ async function assign(req, res) {
             return
         }
         sql = "UPDATE `swaps` SET `bsc_tx` = ? WHERE `uuid` = ?;";
-        conP.query(sql, [req.body.tx, requset_uuid]).then(() => {
+        conP.query(sql, [req.query.tx, req.query.uuid]).then(() => {
             logger.debug(`Completed ${reqInfo}`)
-            res.sendStatus(200);
+            if (req.query.no_redirect) {
+                res.sendStatus(200);
+            } else {
+                res.redirect(`/operation/${req.query.uuid}`);
+            }
         }).catch(reject)
         return
     }
@@ -194,6 +216,43 @@ async function create(req, res) {
             }
         })
     })
+}
+
+router.get('/calculateFees/:uuid', async function (req, res) {
+    try {
+        await calculateFees(req, res)
+    } catch (error) {
+        logger.error(`Failed ${req.path}: ${error}`)
+        res.sendStatus(500)
+    }
+});
+
+async function calculateFees(req, res) {
+    const reqInfo = req.path
+    logger.debug(`Got ${reqInfo}`)
+    if (!uuid.validate(req.params.uuid)) {
+        logger.debug(`Bad request ${reqInfo}`)
+        res.sendStatus(400);
+        return
+    }
+    let sql = "SELECT address, amount FROM `swaps` WHERE `uuid` = ? LIMIT 1;";
+    db.promise().execute(sql, [req.params.uuid])
+        .then(async ([data, fields]) => {
+            if (!data[0]) {
+                logger.debug(`Not found ${reqInfo}`)
+                res.sendStatus(404);
+                return
+            }
+            logger.debug(`Completed ${reqInfo}`)
+
+            res.status(200).json({
+                result: await bsc.calculateFees(data[0].address, data[0].amount)
+            })
+        })
+        .catch(err => {
+            logger.error(`Failed ${reqInfo}: ${err}`)
+            res.sendStatus(500);
+        });
 }
 
 module.exports = router;
